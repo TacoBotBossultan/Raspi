c17046308afa246372f337fd556448642a782005
Your Name <you@example.com>
2025-08-13 12:50:40 +0100

    pus editor config peentru script

--- raspi/src/chassis/id_identifier.sh
+++ raspi/src/chassis/id_identifier.sh
@@ -1,12 +1,23 @@
-#!/bin/bash
+#! /bin/bash
 
 for sysdevpath in $(find /sys/bus/usb/devices/usb*/ -name dev); do
-  (
-    syspath="${sysdevpath%/dev}"
-    devname="$(udevadm info -q name -p $syspath)"
-    [[ "$devname" != "tty"* ]] && exit
-    eval "$(udevadm info -q property --export -p $syspath)"
-    [[ -z "$ID_USB_SERIAL_SHORT" ]] && exit
-    echo "/dev/$devname - $ID_USB_SERIAL_SHORT"
-  )
+	(
+		syspath="${sysdevpath%/dev}"
+		devname="$(udevadm info -q name -p $syspath)"
+		[[ "$devname" != "tty"* ]] && exit
+		eval "$(udevadm info -q property --export -p $syspath)"
+		[[ -z "$ID_USB_SERIAL_SHORT" ]] && exit
+		echo "/dev/$devname - $ID_USB_SERIAL_SHORT"
+	)
 done
+
+# for sysdevpath in $(find /sys/bus/usb/devices/usb*/ -name dev); do
+#   (
+#     syspath="${sysdevpath%/dev}"
+#     devname="$(udevadm info -q name -p $syspath)"
+#     echo "$devname"
+#     eval "$(udevadm info -q property --export -p $syspath)"
+#     [[ -z "$ID_USB_SERIAL_SHORT" ]] && exit
+#     echo "/dev/$devname - $ID_USB_SERIAL_SHORT"
+#   )
+# done
