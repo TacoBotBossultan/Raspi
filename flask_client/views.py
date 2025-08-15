08e4fcf708ea34d7607ec2112919910f308e7536
viktorashi <ioanvictorstan@gmail.com>
2025-08-15 15:15:24 +0300

    Rezolvat probleme cu globale cu din alea

--- flask_client/views.py
+++ flask_client/views.py
@@ -1,4 +1,6 @@
 from flask import render_template
+
+import connect_to_server
 from . import app
 from connect_ghipitty import jesus_take_the_wheel, query_ghiptty
 from connect_to_server import connect_to_rust_server, send_request
@@ -8,6 +10,8 @@ HOST = "127.0.0.1"
 PORT = 8080
 
 socketio = SocketIO(app)
+soseata_rust = "nimic"
+connected = False
 
 
 @app.route("/")
@@ -18,10 +22,9 @@ def index():
 @socketio.on("message")
 def handle_message(msg):
     print("Message from client: " + msg)
-    global connected, soseata
+    global connected, soseata_rust
     if connected:
         res = query_ghiptty(msg)
-        # defining a custom event named 'response'.
         print("Cea zis ghiptty:", res)
         if res != None:
             emit("response", res)
@@ -32,10 +35,9 @@ def handle_message(msg):
             "response",
             "Stai ba ca nici nu e conectat la serveru ala jegos de Rust nuj dc, incerc din nou sa ma contectez ... scrie-mi si tu mai incolo",
         )
-        (soseata, connected) = connect_to_rust_server()
+        soseata_rust, connected = connect_to_rust_server()
 
 
 if __name__ == "__main__":
-    global soseata, connected
-    (soseata, connected) = connect_to_rust_server()
+    soseata_rust, connected = connect_to_rust_server()
     socketio.run(app, debug=True)
